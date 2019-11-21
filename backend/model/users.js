b3b9a1e7ad9f7cad6112d1bdffbe6527c8480bab
ppciesiolkiewicz <ppciesiolkiewicz@gmail.com>
2019-11-21 14:20:17 +1100

    users model added

---
+++ backend/model/users.js
@@ -0,0 +1,31 @@
+const faker = require("faker");
+const loki = require("lokijs");
+const _ = require('lodash');
+
+const db = new loki("MSTS");
+const users = db.addCollection("users", { indices: ["id"] });
+
+const AMOUNT_OF_USERS_TO_GENERATE = 100;
+
+const generateRandomAddress = () => ({
+  country: faker.address.country(),
+  streetName: faker.address.streetName(),
+  streetNumber: faker.random.number(),
+  city: faker.address.city(),
+  postalCode: faker.address.zipCode(),
+  phone: faker.phone.phoneNumber(),
+});
+
+users.insert(
+  _.range(AMOUNT_OF_USERS_TO_GENERATE).map(i => ({
+    id: i.toString(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    shippingAddress: generateRandomAddress(),
+    billingAddress: generateRandomAddress(),
+  }),
+));
+
+module.exports = {
+  getById: id => sers.find({ id })[0],
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
+const users = require('../model/users');
+
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
